790afe914e7e591be6e989ed9285ba790cbf6c8a
zhoufangquan <2542154447@qq.com>
2021-12-07 20:51:24 +0800

    init

---
+++ code/1117.h-2-o-生成.cpp
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=1117 lang=cpp
+ *
+ * [1117] H2O 生成
+ */
+
+// @lc code=start
+class H2O {
+public:
+    H2O() {
+        
+    }
+
+    void hydrogen(function<void()> releaseHydrogen) {
+        
+        // releaseHydrogen() outputs "H". Do not change or remove this line.
+        releaseHydrogen();
+    }
+
+    void oxygen(function<void()> releaseOxygen) {
+        
+        // releaseOxygen() outputs "O". Do not change or remove this line.
+        releaseOxygen();
+    }
+};
+// @lc code=end
+
---
+++ code/19.删除链表的倒数第-n-个结点.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode.cn id=19 lang=python
+#
+# [19] 删除链表的倒数第 N 个结点
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: ListNode
+        :type n: int
+        :rtype: ListNode
+        """
+# @lc code=end
+
