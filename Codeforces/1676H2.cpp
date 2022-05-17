2b62e4ab0480cb080adfa88c4a8e64a302379f09
zhoufangquan <2542154447@qq.com>
2022-05-17 22:27:09 +0800

    归并+统计逆序对

--- Codeforces/1676H2.cpp
+++ Codeforces/1676H2.cpp
@@ -1,22 +1,89 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
+int arr[400005];
 
-void build(int id, int l, int r){
-
+int slove_segmentTree(int n)
+{
+    return 0;
 }
 
-void update(int id, int l, int r, int p, int value){
+long long slove_merge(int n)
+{
+    int len = 1;
+    long long ans = 0;
+    for (; len <= n; len *= 2)
+    {
+        for (int l = 1; l <= n; l += 2 * len)
+        {
+            int i = l + len - 1;
+            int j = min(l + 2 * len - 1, n);
+            if (i == n)
+                continue;
+            while (i >= l && j >= l + len)
+            {
+                if (arr[i] >= arr[j])
+                {
+                    ans += (long long)(j - l - len) + (long long)1;
+                    i--;
+                }
+                else
+                {
+                    j--;
+                }
+            }
+            i = l + len - 1;
+            j = min(l + 2 * len - 1, n);
+            stack<int> temp;
+            while (i >= l && j >= l + len)
+            {
 
+                if (arr[i] >= arr[j])
+                {
+                    temp.push(arr[i]);
+                    i--;
+                }
+                else
+                {
+                    temp.push(arr[j]);
+                    j--;
+                }
+            }
+            while (i >= l)
+            {
+                temp.push(arr[i]);
+                i--;
+            }
+            while (j >= l + len)
+            {
+                temp.push(arr[j]);
+                j--;
+            }
+
+            i = l;
+            while (!temp.empty())
+                arr[i++] = temp.top(), temp.pop();
+        }
+    }
+
+    return ans;
 }
 
-int read(int id, int l, int r, int x, int y){
-
-}
-
-int main(){
-    
-
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int n;
+        cin >> n;
+        for (int i = 1; i <= n; i++)
+        {
+            scanf("%d", arr + i);
+        }
+        long long ans = slove_merge(n);
+        cout << ans << '\n';
+    }
     return 0;
 }
