3df4f74c5bf18b2f3f2c4a279c0f74719e9eb761
zhoufangquan <2542154447@qq.com>
2022-05-22 21:16:17 +0800

    init

---
+++ code/991坏了的计算器.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=991 lang=cpp
+ *
+ * [991] 坏了的计算器
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int brokenCalc(int startValue, int target) {
+
+    }
+};
+// @lc code=end
+
